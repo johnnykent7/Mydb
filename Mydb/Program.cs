699d3dd9cde20557a4bd28ad500e43ef76e57ad0
Ion <ionglavan@mail.ru>
2018-07-10 03:06:32 +0300

    Add project files.

---
+++ Mydb/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mydb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new MydbEntities();
+            video.AddVideo("First Video", DateTime.Now, "Romance");
+            video.spAddVideo("second video", DateTime.Today, "Romance");
+        }
+    }
+}
